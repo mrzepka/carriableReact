dff75627fb9a93b4e5cb95c02f643c84adfce62f
Matt Rzepka <matthewrzepka@gmail.com>
2017-09-19 17:29:09 -0400

    Added a text input and region dropdown for potential users to enter their information.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class RegionDropdown extends Component {
+  render() {
+    return(
+      <div className="RegionDropdown">
+        <select id="region">
+          <option value="NA">NA</option>
+          <option value="EUE">EUE</option>
+          <option value="EUW">EUW</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+class SummonerInput extends Component {
+  render() {
+    return (
+      <div className="SummonerInput">
+        <form action="/temp_loading_page" method="POST">
+          <div>
+            <label for="summoner_name"></label>
+            <input type="text" id="summoner_name" placeholder="SummonerName"></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Let's get carried!</h2>
+          <SummonerInput></SummonerInput>
+          <RegionDropdown></RegionDropdown>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/*
+  This file is just going to be loading components from other React files
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
