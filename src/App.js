342618cde75ed81cb1891e3135a6aaed172ec7ab
Matt Rzepka <matthewrzepka@gmail.com>
2017-09-20 11:57:03 -0400

    Refactored form to be a react form. Leverages states to control data passed into the form.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class RegionDropdown extends Component {
-  render() {
-    return(
-      <div className="RegionDropdown">
-        <select id="region">
-          <option value="NA">NA</option>
-          <option value="EUE">EUE</option>
-          <option value="EUW">EUW</option>
-        </select>
-      </div>
-    );
+class EntryForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      summoner: '',
+      region: 'NA'
+    };
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.summonerChange = this.summonerChange.bind(this);
+    this.regionChange = this.regionChange.bind(this);
   }
-}
 
-class SummonerInput extends Component {
+  handleSubmit(event) {
+    // alert('Summoner submitted: ' + this.state.summoner + ' in ' + this.state.region);
+
+    //This will need to send the data from the form to a database.
+
+    event.preventDefault();
+  }
+
+  summonerChange(event) {
+    this.setState({summoner: event.target.value});
+  }
+
+  regionChange(event) {
+    this.setState({region: event.target.value});
+  }
   render() {
     return (
-      <div className="SummonerInput">
-        <form action="/temp_loading_page" method="POST">
-          <div>
-            <label for="summoner_name"></label>
-            <input type="text" id="summoner_name" placeholder="SummonerName"></input>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="SummonerInput">
+            <input type="text" id="summoner_name" placeholder="SummonerName" onChange={this.summonerChange}></input>
           </div>
+          <div className="RegionDropdown">
+            <select id="region" value={this.state.region} onChange={this.regionChange}>
+              <option value="NA">NA</option>
+              <option value="EUE">EUE</option>
+              <option value="EUW">EUW</option>
+            </select>
+          </div>
+          <input type="submit" value="Submit" />
         </form>
       </div>
     );
@@ -37,8 +57,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>Let's get carried!</h2>
-          <SummonerInput></SummonerInput>
-          <RegionDropdown></RegionDropdown>
+          <EntryForm></EntryForm>
         </div>
       </div>
     );
